60e9866ca281c2f87ab8d799a72e959f5cf09ed2
Marino Missiroli <m.missiroli@cern.ch>
2024-04-06 20:19:16 +0200

    updates for local measurements on srv-b1b07-16-01 (timing server)

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 ./run_240331.sh /users/missirol/test/dev/CMSSW_14_0_0/tmp/240331_ThroughputMeasurements/TimingTest_01/GRun/V3 test240331_GRunV79
+./run_240406.sh /users/missirol/test/dev/CMSSW_14_0_0/tmp/240331_ThroughputMeasurements/TimingTest_01/GRun/V3 test240406_GRunV79
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 export SCRAM_ARCH=el8_amd64_gcc12
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SITECONFIG_PATH="/opt/offline/SITECONF/local"
+#export SITECONFIG_PATH="/opt/offline/SITECONF/local"
 #[ -d CMSSW_14_0_3 ] || cmsrel CMSSW_14_0_3
 #cd CMSSW_14_0_3/src
 #cmsenv
