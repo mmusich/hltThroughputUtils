8308aec4a0dac4128ce0d634430032ec1a90f601
Marino Missiroli <m.missiroli@cern.ch>
2024-05-18 16:41:57 +0200

    added "240518_testCMSHLT3196"

--- customizeHLTforThroughputMeasurements.py
+++ customizeHLTforThroughputMeasurements.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 def customizeHLTforThroughputMeasurements(process):
-
     # remove check on timestamp of online-beamspot payloads
     process.hltOnlineBeamSpotESProducer.timeThreshold = int(1e6)
 
@@ -17,5 +16,56 @@ def customizeHLTforThroughputMeasurements(process):
     # write a JSON file with the timing information
     if hasattr(process, 'FastTimerService'):
         process.FastTimerService.writeJSONSummary = True
+
+    return process
+
+def customizeHLTforCMSHLT3196_baseline(process):
+    process = customizeHLTforThroughputMeasurements(process)
+    return process
+
+def customizeHLTforCMSHLT3196_CCCLooseInAll(process):
+    process = customizeHLTforThroughputMeasurements(process)
+    process.HLTSiStripClusterChargeCutNone = cms.PSet(  value = cms.double( 1620.0 ) )
+    return process
+
+def customizeHLTforCMSHLT3196_CCCLooseInSiStripUnpacker(process):
+    process = customizeHLTforThroughputMeasurements(process)
+    process.hltSiStripRawToClustersFacility.Clusterizer.clusterChargeCut.refToPSet_ = 'HLTSiStripClusterChargeCutLoose'
+    return process
+
+def customizeHLTforCMSHLT3196_CCCLooseInRefToPSet(process):
+    process = customizeHLTforThroughputMeasurements(process)
+
+    def updateRefToPSet(module):
+        ret = []
+        for parName in module.parameterNames_():
+            param = getattr(module, parName)
+            if isinstance(param, cms.VPSet):
+                for pset in param:
+                    ret += updateRefToPSet(pset)
+            elif isinstance(param, cms.PSet):
+                ret += updateRefToPSet(param)
+            elif parName == 'refToPSet_':
+                if param.value() == 'HLTSiStripClusterChargeCutNone':
+                    setattr(module, parName, 'HLTSiStripClusterChargeCutLoose')
+        return ret
+
+    for moduleDict in [
+        process.psets_(),
+        process.es_prefers_(),
+        process.es_producers_(),
+        process.es_sources_(),
+        process.filters_(),
+        process.producers_(),
+        process.analyzers_(),
+        process.switchProducers_(),
+    ]:
+        for moduleLabel_i in moduleDict:
+            try:
+                if getattr(process, moduleLabel_i).type_() == 'SiStripClusterizerFromRaw':
+                    continue
+            except:
+                pass
+            updateRefToPSet(getattr(process, moduleLabel_i))
 
     return process
