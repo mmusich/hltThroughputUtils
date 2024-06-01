2105da0fdf55fa7bdba6cab397ebb6e6dd871ed9
Marino Missiroli <m.missiroli@cern.ch>
2024-06-01 23:41:13 +0200

    updated run_240601_testCMSHLT3137.sh

--- run_240601_testCMSHLT3137.sh
+++ run_240601_testCMSHLT3137.sh
@@ -6,13 +6,25 @@ jobLabel="${1}"
 runNumber=381065
 outDir=/fff/user/"${USER}"/output/hltThroughputUtils
 
-run() {
+run110() {
   [ ! -d "${3}" ] || exit 1
   foo=$(printf "%125s") && echo ${foo// /-} && unset foo
   printf " %s\n" "${3}"
   foo=$(printf "%125s") && echo ${foo// /-} && unset foo
   rm -rf run"${runNumber}"
   ${2}/benchmark "${1}" -E cmsRun -r 4 -j "${4}" -t "${5}" -s "${6}" -e 40100 -g 1 -n --no-cpu-affinity -l "${3}" -k resources.json --tmpdir "${outDir}"/tmp |& tee "${3}".log
+  ./merge_resources_json.py "${3}"/step*/pid*/resources.json > "${3}".json
+  mv "${3}".log "${3}".json "${3}"
+  cp "${1}" "${3}"
+}
+
+run011() {
+  [ ! -d "${3}" ] || exit 1
+  foo=$(printf "%125s") && echo ${foo// /-} && unset foo
+  printf " %s\n" "${3}"
+  foo=$(printf "%125s") && echo ${foo// /-} && unset foo
+  rm -rf run"${runNumber}"
+  ${2}/benchmark "${1}" -E cmsRun -r 4 -j "${4}" -t "${5}" -s "${6}" -e 40100 -g 1 --no-numa-affinity --gpu-affinity --cpu-affinity -l "${3}" -k resources.json --tmpdir "${outDir}"/tmp |& tee "${3}".log
   ./merge_resources_json.py "${3}"/step*/pid*/resources.json > "${3}".json
   mv "${3}".log "${3}".json "${3}"
   cp "${1}" "${3}"
@@ -49,13 +61,25 @@ process = customizeHLTforCMSHLT3137_${jobSubLabel}(process)
 
     ## GPU (no MPS)
     unset CUDA_VISIBLE_DEVICES
-    run "${jobLabel}"_"${jobSubLabel}"_dump.py ./patatrack-scripts "${outDir}"/"${jobDirPrefix}"-gpu 8 32 24
+    run110 "${jobLabel}"_"${jobSubLabel}"_dump.py ./patatrack-scripts "${outDir}"/"${jobDirPrefix}"-n1g1c0-gpu 8 32 24
 
     ## GPU MPS
     unset CUDA_VISIBLE_DEVICES
     ./start-mps-daemon.sh
     sleep 1
-    run "${jobLabel}"_"${jobSubLabel}"_dump.py ./patatrack-scripts "${outDir}"/"${jobDirPrefix}"-gpu_mps 8 32 24
+    run110 "${jobLabel}"_"${jobSubLabel}"_dump.py ./patatrack-scripts "${outDir}"/"${jobDirPrefix}"-n1g1c0-gpu_mps 8 32 24
+    ./stop-mps-daemon.sh
+    sleep 1
+
+    ## GPU (no MPS)
+    unset CUDA_VISIBLE_DEVICES
+    run011 "${jobLabel}"_"${jobSubLabel}"_dump.py ./patatrack-scripts "${outDir}"/"${jobDirPrefix}"-n0g1c1-gpu 8 32 24
+
+    ## GPU MPS
+    unset CUDA_VISIBLE_DEVICES
+    ./start-mps-daemon.sh
+    sleep 1
+    run011 "${jobLabel}"_"${jobSubLabel}"_dump.py ./patatrack-scripts "${outDir}"/"${jobDirPrefix}"-n0g1c1-gpu_mps 8 32 24
     ./stop-mps-daemon.sh
     sleep 1
 
